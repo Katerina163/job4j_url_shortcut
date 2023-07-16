2b03f882e581a872f53eeb3a0fec826d5ddf239f
Katerina <Nefedova.Katia@yandex.ru>
2023-07-16 11:08:49 +0400

    добавление WebSecurity

---
+++ src/main/java/ru/job4j/url/shortcut/configuration/WebSecurity.java
@@ -0,0 +1,54 @@
+package ru.job4j.url.shortcut.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import ru.job4j.url.shortcut.filter.JWTAuthenticationFilter;
+import ru.job4j.url.shortcut.filter.JWTAuthorizationFilter;
+
+import static ru.job4j.url.shortcut.filter.JWTAuthenticationFilter.REGISTER_URL;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService service;
+    private final BCryptPasswordEncoder encoder;
+
+    public WebSecurity(UserDetailsService simpleWebsiteService, BCryptPasswordEncoder encoder) {
+        service = simpleWebsiteService;
+        this.encoder = encoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, REGISTER_URL).permitAll()
+                .antMatchers(HttpMethod.POST, "/registration").permitAll()
+                .antMatchers("/redirect/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(service).passwordEncoder(encoder);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+}
