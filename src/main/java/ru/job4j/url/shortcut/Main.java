5c083d6bedd6311200829338b3bee0c4241abefb
Katerina <Nefedova.Katia@yandex.ru>
2023-07-16 11:02:27 +0400

    корректировка

--- src/main/java/ru/job4j/url/shortcut/Main.java
+++ src/main/java/ru/job4j/url/shortcut/Main.java
@@ -2,6 +2,8 @@ package ru.job4j.url.shortcut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Main {
@@ -10,4 +12,8 @@ public class Main {
 		SpringApplication.run(Main.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder getbCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder(10);
+	}
 }
