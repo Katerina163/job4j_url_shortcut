6404fb51ec95dfb154a13c3eccc224fe129c75be
Katerina Nefedova <Nefedova.Katia@yandex.ru>
2023-07-23 13:27:59 +0400

    корректировка

--- src/main/resources/db/scripts/002_dml_insert.sql
+++ src/main/resources/db/scripts/002_dml_insert.sql
@@ -1,8 +1,8 @@
 --liquibase formatted sql
 --changeset Katerina163:1
-insert into website(login, password, domain) values('KFod8dhe*Ed', 'flwei9IF*Deri32', 'yandex.ru');
+insert into website(login, password, domain) values('b49a5e2c-75e2-4a1f-942c-4d0c1e25b25a', 'flwei9IF*Deri32', 'yandex.ru');
 --rollback truncate table website;
 
 --changeset Katerina163:2
-insert into page(path, code, count, website_id) values('https://yandex.ru/pogoda/', 'dDKS8sh&S', 0, 1);
+insert into page(path, code, count, website_id) values('https://yandex.ru/pogoda/', 'YXkugmfM&S', 0, 1);
 --rollback truncate table page;
